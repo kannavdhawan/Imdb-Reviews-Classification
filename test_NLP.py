2df679d508de5e68a2a74f274c4b3626e1245bfa
Kannav Dhawan <k4dhawan@uwaterloo.ca>
2020-07-23 00:06:38 +0000

    data create

---
+++ test_NLP.py
@@ -0,0 +1,14 @@
+# import required packages
+
+
+# YOUR IMPLEMENTATION
+# Thoroughly comment your code to make it easy to follow
+
+
+if __name__ == "__main__": 
+    pass
+	# 1. Load your saved model
+
+	# 2. Load your testing data
+
+	# 3. Run prediction on the test data and print the test accuracy
---
+++ train_NLP.py
@@ -0,0 +1,16 @@
+# import required packages
+
+
+# YOUR IMPLEMENTATION
+# Thoroughly comment your code to make it easy to follow
+
+
+if __name__ == "__main__": 
+    pass
+	# 1. load your training data
+
+	# 2. Train your network
+	# 		Make sure to print your training loss and accuracy within training to show progress
+	# 		Make sure you print the final training accuracy
+
+	# 3. Save your model
